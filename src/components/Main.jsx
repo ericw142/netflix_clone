5a0effa64682deb5cdb45f80496e6babdd02aa64
ericw142 <eric.h.warshawsky@gmail.com>
2024-02-13 11:28:01 -0500

    row component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import requests from '../Requests'
 import axios from 'axios'
 
-const baseURL = 'https://image.tmdb.org/t/p/';
+const baseURL = 'https://image.tmdb.org/t/p/original/';
 
 const Main = () => {
     const [movies, setMovies] = useState([])
@@ -25,7 +25,7 @@ const Main = () => {
         <div className='w-full h-[550px] text-white'>
             <div className='w-full h-full'>
                 <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
-                <img className='w-full h-full object-cover' src={`${baseURL}original/${movie?.backdrop_path}`} alt={movie?.title} />
+                <img className='w-full h-full object-cover' src={`${baseURL}${movie?.backdrop_path}`} alt={movie?.title} />
                 <div className='absolute w-full top-[20%] p-4 md:p-8'>
                     <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
                     <div className='my-4'>
