5c1a4eeedef434655fbec6e1c22486024a18112d
ericw142 <eric.h.warshawsky@gmail.com>
2024-02-16 11:09:37 -0500

    alt tags

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -22,7 +22,7 @@ const Signup = () => {
     return (
         <>
             <div className='w-full h-screen'>
-                <img className='hidden sm:block absolute w-full h-full object-cover' src="https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg"/>
+                <img className='hidden sm:block absolute w-full h-full object-cover' src="https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg" alt="movie_background"/>
                 <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
                 <div className='fixed w-full px-4 py-24 z-50'>
                     <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
